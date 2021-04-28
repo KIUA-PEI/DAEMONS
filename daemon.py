bfe5e43afad3d54daeedba9829ce99430eb650f9
Pedro Abreu <pedro.abreu@ua.pt>
2021-04-28 22:04:03 +0100

    pending

--- daemon.py
+++ daemon.py
@@ -24,10 +24,19 @@ def ten_sec_job(producer):
 def thirty_sec_job():
     print("this job runs every 30 sec")
 
-def twenty_min_job(producer, parkkey):
-    parking = parking_data()
+def twenty_min_job(producer, token, parkkey):
+    parking = parking_data('Bearer ' + token)
+    if parking == -1:
+        token = get_acess_token()
+        parking = parking_data('Bearer ' + token)
     parkkey = parkkey + 1
     producer.send("parking", value={"PARK"+str(parkkey) : parking})
+
+def get_acess_token():
+    request_token = requests.post('https://wso2-gw.ua.pt/token?grant_type=client_credentials&state=123&scope=openid', \
+    headers = {'Content-Type': 'application/x-www-form-urlencoded','Authorization': 'Basic al9tR25keEsyV0xLRVVLYkdya1g3bjF1eEFFYTpCcnN6SDhvRjlRc0hSamlPQUMxRDlaZTBJbG9h'})
+    if r.status_code == 200:
+        return 'Bearer ' + request_token.json()['access_token']
 
 
 def main():
@@ -40,18 +49,20 @@ def main():
         'coalesce': False,
         'max_instances': 3
     }
-    # cuidado com a timezone
     scheduler.configure(job_defaults=job_defaults)
 
     # start Kafka Python Client
     producer = KafkaProducer(bootstrap_servers=['13.69.49.187:9092'], value_serializer=lambda x: json.dumps(x).encode('utf-8'))
     parkkey = 0
 
+    # get primecoreAPI access token
+    token = get_acess_token()
+
     # add jobs
     scheduler.add_job(hour_job, trigger="interval", hours=1, id="1hourjob")
     scheduler.add_job(ten_sec_job, trigger="interval", args=[producer], seconds=10, id="10secjob")
     scheduler.add_job(thirty_sec_job, trigger="interval", seconds=30, id="30secjob")
-    scheduler.add_job(twenty_min_job, trigger="interval", args=[producer, parkkey], minutes=20, id="20minjob", next_run_time=datetime.now())
+    scheduler.add_job(twenty_min_job, trigger="interval", args=[producer, token, parkkey], minutes=20, id="20minjob", next_run_time=datetime.now())
 
     # start the scheduler
     scheduler.start()
--- metrics/wirelessUsers.py
+++ metrics/wirelessUsers.py
@@ -1,12 +1,16 @@
 import requests
+from consts import *
 
-def data_wirelessUsers():
+def data_wirelessUsers(token):
     pass
-    # r = requests.get("https://wso2-gw.ua.pt/primecore_primecore-ws/1.0.0/AccessPoint/deti-ap03/TotalUsernames")
+    # for dep in DEP:
+    #     i = 0
+    #     while i < MAX_AP[dep]:
+    #         r = requests.get("https://wso2-gw.ua.pt/primecore_primecore-ws/1.0.0/AccessPoint/"+\
+    #         str(DEP[dep])+(str(i) if i > 9 and not i == 0 else "0" + str(i))+"/TotalUsernames", headers={'Authorization': token})
+    #         print(r.status_code)
+    #         print(r.text)
+    #         i = i + 1
 
 
-def get_acess_token():
-    request_token = requests.post('https://wso2-gw.ua.pt/token?grant_type=client_credentials&state=123&scope=openid', \
-    headers = {'Content-Type': 'application/x-www-form-urlencoded','Authorization': 'Basic al9tR25keEsyV0xLRVVLYkdya1g3bjF1eEFFYTpCcnN6SDhvRjlRc0hSamlPQUMxRDlaZTBJbG9h'})
-    if r.status_code == 200:
-        return 'Bearer ' + request_token.json()['access_token']
+# data_wirelessUsers('Bearer ' + "31ec0e34-0fc7-39a2-acd7-ef69715acda3")
